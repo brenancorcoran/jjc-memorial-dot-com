288b9cb2e9de12ff6294dd576956be2c6a1f8b47
Brenan Corcoran <bcorcoran1@luc.edu>
2025-08-11 20:04:38 -0500

    created 2025 photos html page and fixed the countdown to JJC 2026

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to EST TimeZone
-var countDownDate = new Date('Aug 1, 2025 13:00:00 GMT-05:00').getTime();
+var countDownDate = new Date('Aug 7, 2026 13:00:00 GMT-05:00').getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
